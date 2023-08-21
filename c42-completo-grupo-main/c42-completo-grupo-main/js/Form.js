086cf6bfad142e3fa9a869fbb6acda312989f047
odraud3 <131033176+odraud3@users.noreply.github.com>
2023-08-21 18:20:07 -0300

    Add files via upload

---
+++ c42-completo-grupo-main/c42-completo-grupo-main/js/Form.js
@@ -0,0 +1,54 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Digite seu nome");
+    this.playButton = createButton("Jogar");
+    this.titleImg = createImg("./assets/title.png", "nome do jogo");
+    this.greeting = createElement("h2");
+     //para poder ter outra frase 
+     this.nick="";
+  }
+
+  setElementsPosition() {
+    this.titleImg.position(120,50);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+  }
+
+  setElementsStyle() {
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  }
+
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+
+  handleMousePressed() {
+    this.playButton.mousePressed(() => {
+      this.input.hide();
+      this.playButton.hide();
+      playerCount += 1;
+      this.nick=this.input.value()===""? "player"+playerCount:this.input.value();
+      var message = `
+      Olá ${this.input.value()}
+      </br>espere o outro jogador entrar...`;
+      this.greeting.html(message);
+      player.name = this.nick;
+      player.index = playerCount;
+      player.addPlayer();
+      player.updateCount(playerCount);
+      player.getDistance();
+    });
+  }
+
+  display() {
+    this.setElementsPosition();
+    this.setElementsStyle();
+    this.handleMousePressed();
+  }
+}
---
+++ c42-completo-grupo-main/c42-completo-grupo-main/js/Game.js
@@ -0,0 +1,230 @@
+class Game {
+  constructor() {
+    this.resetTitle = createElement("h2");
+    this.resetButton = createButton("");
+  
+    this.leaderBoardTitle = createElement("h2")
+    this.leader1 = createElement("h2")
+    this.leader2 = createElement("h2")
+  }
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite(width / 2 - 50, height - 100);
+    car1.addImage("careo1", car1_img);
+    car1.scale = 0.07;
+
+    car2 = createSprite(width / 2 + 100, height - 100);
+    car2.addImage("carro2", car2_img);
+    car2.scale = 0.07;
+
+    cars = [car1, car2];
+
+    fuels = new Group();
+    powerCoins = new Group();
+
+    // Adicione o sprite de combustível ao jogo
+    this.addSprites(fuels, 4, fuelImage, 0.02);
+
+    // Adicione o sprite de moeda ao jogo
+    this.addSprites(powerCoins, 18, powerCoinImage, 0.09);
+  }
+
+  addSprites(spriteGroup, numberOfSprites, spriteImage, scale) {
+    for (var i = 0; i < numberOfSprites; i++) {
+      var x, y;
+
+      x = random(width / 2 + 150, width / 2 - 150);
+      y = random(-height * 4.5, height - 400);
+
+      var sprite = createSprite(x, y);
+      sprite.addImage("sprite", spriteImage);
+
+      sprite.scale = scale;
+      spriteGroup.add(sprite);
+    }
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+   
+    this.resetTitle.html("Reiniciar o Jogo");
+    this.resetTitle.class("resetText");
+    this.resetTitle.position(width / 2 + 200, 40);
+
+    this.resetButton.class("resetButton");
+    this.resetButton.position(width / 2 + 230, 100);
+
+    this.leaderBoardTitle.html("Placar")
+    this.leaderBoardTitle.class("resetText")
+    this.leaderBoardTitle.position(width/3 -80,40)
+
+    this.leader1.class("leadersText")
+    this.leader1.position(width/3 -60,80)
+
+    this.leader2.class("leadersText")
+    this.leader2.position(width/3 -60,130)
+
+  }
+
+
+
+  play() {
+    this.handleElements();
+    this.handleResetButton();
+    Player.getPlayersInfo();
+    player.getcarsAtEnd();
+
+    if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6);
+
+      //índice da matriz
+      var index = 0;
+      for (var plr in allPlayers) {
+        //adicione 1 ao índice para cada loop
+        index = index + 1;
+
+        //use os dados do banco de dados para exibir os carros nas direções x e y
+        var x = allPlayers[plr].positionX;
+        var y = height - allPlayers[plr].positionY;
+
+        cars[index - 1].position.x = x;
+        cars[index - 1].position.y = y;
+        
+        if (index === player.index) {
+          stroke(10);
+          fill("dodgerblue");
+          ellipse(x, y, 60, 60);
+
+          this.handleFuel(index);
+          this.handlePowerCoins(index);
+          
+          // Altere a posição da câmera na direção y
+          camera.position.x = cars[index - 1].position.x;
+          camera.position.y = cars[index - 1].position.y;
+
+        }
+      }
+
+      this.handlePlayerControls();
+
+      drawSprites();
+    }
+  }
+
+  handleFuel(index) {
+    // Adicione o combustível
+    cars[index - 1].overlap(fuels, function(collector, collected) {
+      player.fuel = 185;
+      //collected (coletado) é o sprite no grupo de colecionáveis que desencadeia
+      //o evento
+      collected.remove();
+    });
+  }
+
+  handlePowerCoins(index) {
+    cars[index - 1].overlap(powerCoins, function(collector, collected) {
+      player.score += 21;
+      player.update();
+      //ccollected (coletado) é o sprite no grupo de colecionáveis que desencadeia
+      //o evento
+      collected.remove();
+    });
+  }
+
+handleResetButton() {
+  this.resetButton.mousePressed(() => {
+    database.ref("/").set({
+      playerCount: 0,
+      gameState: 0,
+      carsAtEnd: 0,
+      players: {}
+    });
+    window.location.reload();
+  });
+
+}
+
+showLeaderBoard(){
+  var leader1,leader2
+  var players = Object.values(allplayers)
+
+  if ((players[0].rank === 0 && players[1].rank === 0)|| 
+  players[0].rank === 1) {
+    leader1 =
+    players[0].rank +
+    "&emsp;"+
+    players[0].name+
+    "&emsp;"+
+    players[0].score;
+
+    leader2 =
+    players[1].rank +
+    "&emsp;" +
+    players[1].name+
+    "&emsp;"+
+    players[1].score;
+
+
+    
+  }
+  if (players[1].rank === 1 ) {
+    leader1 =
+    players[1].rank +
+    "&emsp;"+
+    players[1].name+
+    "&emsp;"+
+    players[1].score;
+
+    leader2 =
+    players[0].rank +
+    "&emsp;" +
+    players[0].name+
+    "&emsp;"+
+    players[0].score;
+  }
+
+  this.leader1.html(leader1)
+  this.leader2.html(leader2)
+
+}
+
+handlePlayerControls() {
+  if (keyIsDown(UP_ARROW) || keyDown("w")) {
+    player.positionY += 10;
+    player.update();
+  }
+
+  if (keyIsDown(LEFT_ARROW) && player.positionX > width / 3 - 50 || keyDown("a") && player.positionX>width/3-50) {
+    player.positionX -= 5;
+    player.update();
+  }
+
+  if (keyIsDown(RIGHT_ARROW) && player.positionX < width / 2 + 300 || keyDown("d") && player.positionX<width/2+300) {
+    player.positionX += 5;
+    player.update();
+  }
+}
+
+
+
+}
